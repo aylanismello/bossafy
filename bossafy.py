f08268ca3c599dff1bcc11a63c89f0030060e802
Aylan Mello <amello@malwarebytes.com>
2017-08-12 15:54:03 -0700

    better code organizaiton

--- bossafy.py
+++ bossafy.py
@@ -1,29 +1,60 @@
-import subprocess
-import json
-import os.path
+import subprocess, json
+import os
+from selenium import webdriver
+from collections import defaultdict
 from IPython import embed
 
 class Bossafy(object):
-    ARTISTS_FILE_NAME = './data/config.json'
+    ARTISTS_FILE_PATH = './data/config.json'
+    CHORDS_FILE_PATH = './data/metadata/chord_dict.json'
+    SCRAPED_SONGS_DIR_PATH = './data/song_scrapes/'
 
     def __init__(self):
-        with open(self.ARTISTS_FILE_NAME) as data_file:
-            for genre in json.load(data_file)['artists'].keys():
-                self.scrape_all_artists(genre)
+        self.scrape_songs()
+        self.scrape_chord_definitions()
+
+    def open_or_create_chord_dict(self):
+        try:
+            with open(self.CHORDS_FILE_PATH, 'r') as data_file:
+                chord_dict = json.load(data_file)
+        except:
+            print 'could not open %s, making new file' % (self.CHORDS_FILE_PATH)
+            chord_dict = {}
+        return chord_dict
+
+
+    def scrape_chord_definitions(self):
+        self.chord_dict = self.open_or_create_chord_dict()
+        self.driver = webdriver.PhantomJS()
+        urls_dict = {}
+
+        for filename in os.listdir(self.SCRAPED_SONGS_DIR_PATH):
+            with open("%s%s" % (self.SCRAPED_SONGS_DIR_PATH, filename), 'r') as scraped_song_file:
+                urls_dict[filename.split('.')[0]] = [song['url'] for song in json.load(scraped_song_file)]
+
+        # create/add to chord_dict
+        for _, urls in urls_dict.iteritems():
+            for url in urls:
+                self.add_song_chords_to_dict(url)
+            break
+
+        # write chord_dict to .json
+        with open(self.CHORDS_FILE_PATH, 'w') as data_file:
+            json.dump(self.chord_dict, data_file, sort_keys=True, indent=2)
+
+
+    def scrape_songs(self):
+        with open(self.ARTISTS_FILE_PATH) as data_file:
+            self.all_artists = json.load(data_file)['artists']
+
+        for genre in self.all_artists.keys():
+            self.scrape_all_artists(genre)
 
     def scrape_all_artists(self, genre):
-        for artist in self.get_artists(genre):
+        for artist in self.all_artists[genre]:
             formatted_artist = '-'.join(artist.lower().split(' '))
             url = "https://www.cifraclub.com.br/%s" % (formatted_artist)
             self.scrape_chords(formatted_artist, url)
-
-    def get_artists(self, genre):
-        with open(self.ARTISTS_FILE_NAME) as data_file:
-            try:
-                artists = json.load(data_file)['artists'][genre]
-            except:
-                print 'could not open %s' % (genre)
-        return artists
 
     def scrape_chords(self, artist, url):
         # check if this file exists first
@@ -34,5 +65,22 @@ class Bossafy(object):
         else:
             print '%s already scraped!' % (NEW_FILE_NAME)
 
+    def add_song_chords_to_dict(self, url):
+        self.driver.get(url)
+        chords = self.driver.find_elements_by_css_selector('.chord')
+        for chord in chords:
+            try:
+                chord_name = chord.find_element_by_css_selector('strong').text
+                chord_tab = chord.get_attribute('data-mount')
+            except:
+                # could not get chord name or data-mount (chord tab)!
+                continue
+
+            if not chord_tab or not chord_name:
+                # wtf... chord tab or chord nameis nothing! next!
+                continue
+
+            self.chord_dict[chord_name] = chord_tab
+        print 'added chord-tab definitions from ' + url
 
 Bossafy()
