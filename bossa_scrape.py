63df06ab1fb124ea50be762c770929b0080bfe48
Aylan Mello <aylanismello@gmail.com>
2017-08-12 01:39:22 -0700

    cleaned up, added selenium with headless phantomjs browswer to get chords' tab conversions

--- bossa_scrape.py
+++ bossa_scrape.py
@@ -1,21 +1,25 @@
 import scrapy
+import re
+
+ARTIST_NAME = 'pixinguinha'
 
 class BossaScrapeSpider(scrapy.Spider):
   name = 'bossa_scrape_spider'
-  start_urls = ['https://www.cifraclub.com.br/tom-jobim/wave/',
-   'https://www.cifraclub.com.br/tom-jobim/o-morro-nao-tem-vez/']
+  start_urls = ['https://www.cifraclub.com.br/' + ARTIST_NAME]
 
   def parse(self, response):
-    title = response.css('.g-side-ad h1::text').extract_first()
-    yield { 'title': title }
-    artist = response.css('.g-side-ad h2::text').extract_first()
-    yield { 'artist': artist }
-    key = response.xpath('//span[@id="cifra_tom"]/a/text()').extract_first()
-    yield { 'key': key }
-    chords = response.xpath('//pre//b/text()').extract()
+      title = response.css('.g-side-ad h1::text').extract_first()
+      if title:
+        #   we know the artist already
+        #   artist = response.css('.g-side-ad h2::text').extract_first()
+          url = response.url
+          key = response.xpath('//span[@id="cifra_tom"]/a/text()').extract_first()
+          chords = response.xpath('//pre//b/text()').extract()
+          yield { 'title': title, 'artist': ARTIST_NAME, 'key': key, 'chords': chords, 'url': url }
+      else:
+          hrefs = response.css('.art_musics a::attr("href")').extract()
+          urls = set([href for href in hrefs if (re.match( r'/' + ARTIST_NAME + '/([a-z0-9\-]+)/$', href ))])
+          for url in urls:
+              yield response.follow(url, self.parse)
 
-
-    for chord in chords:
-      yield {
-        'chord': chord
-      }
+    #   we have entered the
---
+++ chord_dictionary_scrape.py
@@ -0,0 +1,61 @@
+import json
+from selenium import webdriver
+from collections import defaultdict
+
+driver = webdriver.PhantomJS()
+CHORDS_FILE_NAME = './data/chord_dict.json'
+SCRAPED_SONGS = './data/chico.json'
+
+
+
+def add_song_chords_to_dict(chords, chord_tab_dict):
+        # chord_tab_dict = {}
+    for chord in chords:
+        try:
+            chord_name = chord.find_element_by_css_selector('strong').text
+            chord_tab = chord.get_attribute('data-mount')
+        except:
+            # print 'could not get chord name or data-mount (chord tab)!'
+            continue
+
+        if not chord_tab or not chord_name:
+            # print 'wtf... chord tab or chord nameis nothing! next!'
+            continue
+
+        chord_tab_dict[chord_name] = chord_tab
+    return chord_tab_dict
+
+
+
+def chords_in_tab_form():
+    try:
+        with open(SCRAPED_SONGS, 'r') as file_data:
+            urls = []
+            scraped_songs = json.load(file_data)
+
+            for song in scraped_songs:
+                urls.append(song['url'])
+    except:
+        print 'CANNOT OPEN' + SCRAPED_SONGS
+
+    try:
+        with open(CHORDS_FILE_NAME, 'r') as file_data:
+            chord_tab_dict = json.load(file_data)
+    except:
+        print 'could not open' + CHORDS_FILE_NAME + ', making new file'
+        chord_tab_dict = {}
+
+    for url in urls:
+        driver.get(url)
+        chords = driver.find_elements_by_css_selector('.chord')
+        chord_tab_dict = add_song_chords_to_dict(chords, chord_tab_dict)
+        print 'added songs from ' + url
+
+    with open(CHORDS_FILE_NAME, 'w') as file_data:
+        json.dump(chord_tab_dict, file_data, sort_keys=True, indent=2)
+
+
+
+
+
+chords_in_tab_form()
